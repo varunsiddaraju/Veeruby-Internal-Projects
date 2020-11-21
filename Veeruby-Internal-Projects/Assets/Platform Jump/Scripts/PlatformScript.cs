b64cfccdf8e1126f0ebb5a15a3fb0578e0ad8006
Vinayak <vinay@veeruby.com>
2020-11-21 13:17:14 +0530

    content in scene deleted and only prefabs kept

--- Veeruby-Internal-Projects/Assets/Platform Jump/Scripts/PlatformScript.cs
+++ Veeruby-Internal-Projects/Assets/Platform Jump/Scripts/PlatformScript.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class PlatformScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float move_Speed = 2f;
+    public float bound_Y = 6f;
+
+    public bool moving_Platform_Left, moving_Platform_Right, breakable_Platform, spike_Platform, standerd_Platform;
+
+    private Animator anim;
+
+
+
+
+
 }
