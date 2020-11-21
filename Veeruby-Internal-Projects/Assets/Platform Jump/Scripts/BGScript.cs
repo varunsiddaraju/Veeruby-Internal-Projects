afc36d9efbe55215994f1ff95ae641833cba2d0c
Vinayak <vinay@veeruby.com>
2020-11-21 11:53:39 +0530

    BG made to scroll

---
+++ Veeruby-Internal-Projects/Assets/Platform Jump/Scripts/BGScript.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScript : MonoBehaviour
+{
+    public float scrollSpeed = 0.3f;
+    private MeshRenderer mesh_Renderer;
+
+    public void Awake()
+    {
+        mesh_Renderer = GetComponent<MeshRenderer>();
+    }
+
+    public void Update()
+    {
+        Scroll();
+    }
+
+    private void Scroll()
+    {
+        Vector2 offset = mesh_Renderer.sharedMaterial.GetTextureOffset("_MainTex");
+        offset.y += Time.deltaTime * scrollSpeed;
+        mesh_Renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+}
